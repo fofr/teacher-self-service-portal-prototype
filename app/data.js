96d970ba502d4cb45efdd0b8b2837c3e0df08262
Paul Hayes <fofr@users.noreply.github.com>
2022-03-17 14:53:03 +0000

    Add forms for editing nino, email and phone numbers

--- app/data.js
+++ app/data.js
@@ -23,5 +23,22 @@ export default {
     line1: '1 Street Name',
     town: 'Town',
     postcode: 'SW1 1AA'
+  },
+  redacted: {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    trn: '1234567',
+    dob: '31/03/1950',
+    nino: 'Q*******C',
+    email: 'ja****oe@*******.com',
+    title: 'Mrs.',
+    previousSurnames: ['Bloggs'],
+    primaryContactNumber: '07*******89',
+    otherContactNumber: '01*******54',
+    address: {
+      line1: '1 Street Name',
+      town: 'Town',
+      postcode: 'SW1 1AA'
+    }
   }
 }
